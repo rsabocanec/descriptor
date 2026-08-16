b4cb95a6ea350c23092bc63a94e789abdbcb4941
rsabocanec <robert.sabocanec@gmail.com>
2026-08-16 18:41:44 +0200

    * Addedd open() and close() methods for the shared_memory class

--- shared_memory.cpp
+++ shared_memory.cpp
@@ -1,1 +1,32 @@
 #include "shared_memory.h"
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int32_t rsabocanec::shared_memory::open(std::string_view path, int32_t flags, int32_t mode/* = 0*/) noexcept {
+    descriptor_ = ::shm_open(path.data(), flags, mode);
+
+    if (descriptor_.load() == -1) {
+        return errno;
+    }
+
+    path_ = path;
+
+    return 0;
+}
+
+int32_t rsabocanec::shared_memory::close() noexcept {
+    int32_t result = file::close();
+
+    if (!path_.empty()) {
+        auto const unlink_result = ::shm_unlink(path_.c_str());
+        if (unlink_result == -1) {
+            return errno;
+        }
+ 
+        path_.clear();
+    }
+
+    return result;
+}
--- shared_memory.h
+++ shared_memory.h
@@ -3,8 +3,11 @@
 
 #pragma once
 
+#include "file.h"
+
 namespace rsabocanec {
-class shared_memory {
+class shared_memory : public file {
+    std::string path_; // Need to store the path for unlinking the shared memory object
 public:
     shared_memory() = default;
     shared_memory(const shared_memory&) = delete;
@@ -12,6 +15,12 @@ public:
     shared_memory& operator=(const shared_memory&) = delete;
     shared_memory& operator=(shared_memory&&) = delete;
     virtual ~shared_memory() = default;
+
+    [[nodiscard]] int32_t open( std::string_view path, 
+                                int32_t flags = 0102 /* O_RDWR | O_CREAT*/, 
+                                int32_t mode = 0644 /*S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH*/) noexcept final override;
+
+    [[nodiscard]] int32_t close() noexcept final override;
 };
 }
 #endif //RSABOCANEC_SHARED_MEMORY_H
